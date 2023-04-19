b33e18942601a511593d80585550a06350999406
George Liu (eva2000) <eva2000@centminmod.com>
2023-04-19 20:28:11 +1000

    update extension save the input text
    update extension save the input text in the chrome.storage.sync and restore it when the popup is reopened to ensure when you nagivate away from popup window, that input text is retained

--- popup.js
+++ popup.js
@@ -6,4 +6,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const popupWidth = data.popupWidth || 600;
     document.documentElement.style.width = `${popupWidth}px`;
   });
+
+  slicerIframe.addEventListener('load', () => {
+    // Restore the saved input text
+    chrome.storage.sync.get('inputText', (data) => {
+      slicerIframe.contentWindow.postMessage({ action: 'restore', inputText: data.inputText || '' }, '*');
+    });
+
+    // Listen for messages from slicer.centminmod.com
+    window.addEventListener('message', (event) => {
+      if (event.data.action === 'save') {
+        chrome.storage.sync.set({ inputText: event.data.inputText });
+      }
+    }, false);
+  });
 });
