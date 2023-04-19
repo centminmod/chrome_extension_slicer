37eb776c6521159848367d66eb35c8305d58098e
George Liu (eva2000) <eva2000@centminmod.com>
2023-04-19 22:30:06 +1000

    autosave to localstorage input

--- popup.js
+++ popup.js
@@ -16,18 +16,22 @@ document.addEventListener('DOMContentLoaded', () => {
       slicerIframe.contentWindow.postMessage({ action: 'restore', inputText: data.inputText || '' }, '*');
     });
 
-    // Listen for messages from slicer.centminmod.com
-    window.addEventListener('message', (event) => {
+    // Add event listener for message event
+    slicerIframe.contentWindow.addEventListener('message', (event) => {
       console.log('Message received:', event.data); // Diagnostic message
 
       if (event.data.action === 'save') {
         console.log('Saving input text:', event.data.inputText); // Diagnostic message
         chrome.storage.sync.set({ inputText: event.data.inputText });
-      }
 
-      // Save the updated input value to local storage
-      localStorage.setItem('savedInput', event.data.inputText);
-      
-    }, false);
+        // Save the updated input value to local storage
+        try {
+          localStorage.setItem('savedInput', event.data.inputText);
+          console.log('Setting localStorage.savedInput to:', event.data.inputText);
+        } catch (e) {
+          console.error('Failed to save input text to local storage:', e);
+        }
+      }
+    });
   };
 });
