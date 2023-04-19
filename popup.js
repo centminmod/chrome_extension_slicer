0e0442a7779424e06d779b8b9b96e8680623094b
George Liu (eva2000) <eva2000@centminmod.com>
2023-04-19 21:31:20 +1000

    update popup.js

--- popup.js
+++ popup.js
@@ -7,17 +7,27 @@ document.addEventListener('DOMContentLoaded', () => {
     document.documentElement.style.width = `${popupWidth}px`;
   });
 
-  slicerIframe.addEventListener('load', () => {
+  slicerIframe.onload = () => {
+    console.log('Iframe content loaded'); // Diagnostic message
+
     // Restore the saved input text
     chrome.storage.sync.get('inputText', (data) => {
+      console.log('Restoring input text:', data.inputText || ''); // Diagnostic message
       slicerIframe.contentWindow.postMessage({ action: 'restore', inputText: data.inputText || '' }, '*');
     });
 
     // Listen for messages from slicer.centminmod.com
     window.addEventListener('message', (event) => {
+      console.log('Message received:', event.data); // Diagnostic message
+
       if (event.data.action === 'save') {
+        console.log('Saving input text:', event.data.inputText); // Diagnostic message
         chrome.storage.sync.set({ inputText: event.data.inputText });
       }
+
+      // Save the updated input value to local storage
+      localStorage.setItem('savedInput', event.data.inputText);
+      
     }, false);
-  });
+  };
 });
