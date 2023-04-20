a90fc65d8093250c729a41cf1f17654c20b4573e
George Liu (eva2000) <eva2000@centminmod.com>
2023-04-21 04:43:41 +1000

    remove no longer needed localstorage code & update manifest.json

--- popup.js
+++ popup.js
@@ -1,17 +1,3 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === 'save') {
-    console.log('Saving input text:', request.inputText);
-    chrome.storage.sync.set({ inputText: request.inputText });
-
-    try {
-      localStorage.setItem('savedInput', request.inputText);
-      console.log('Setting localStorage.savedInput to:', request.inputText);
-    } catch (e) {
-      console.error('Failed to save input text to local storage:', e);
-    }
-  }
-});
-
 document.addEventListener('DOMContentLoaded', () => {
   // Load the saved width value and set it to the popup
   chrome.storage.sync.get('popupWidth', (data) => {
